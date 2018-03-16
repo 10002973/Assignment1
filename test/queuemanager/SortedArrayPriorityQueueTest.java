d76c78f67fd497048de4860bdd5562dc4a063667
Heather <Heather@DESKTOP-PJVST6L>
2018-03-16 12:35:46 +0000

    Updated comments ready for submission.

--- test/queuemanager/SortedArrayPriorityQueueTest.java
+++ test/queuemanager/SortedArrayPriorityQueueTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package queuemanager;
 
 import org.junit.After;
@@ -16,11 +10,10 @@ import static org.junit.Assert.*;
 /**
  *
  * @author Heather Taylor-Stanley 10002973
+ * 
+ * This class tests the sorted array priority queue implementation.
  */
 public class SortedArrayPriorityQueueTest extends PriorityQueueTest{
-    PriorityItem item1;
-    PriorityItem item2;
-    PriorityItem item3;
 
     public SortedArrayPriorityQueueTest() {
     }
@@ -28,7 +21,6 @@ public class SortedArrayPriorityQueueTest extends PriorityQueueTest{
     /*
      * Sets up a new instance. 
      */
-
     @Before
     @Override
     public void setUp(){
@@ -47,34 +39,33 @@ public class SortedArrayPriorityQueueTest extends PriorityQueueTest{
      */
     @Test
     public void testAdd() throws Exception {
-        System.out.println("add");
+        System.out.println("Add item to middle of array");
         instance.add("John Doe", 10);
         instance.add("Jane Woods", 30);
         System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
         Object item = ("Jeff Morrison");
         int priority = 20;
         instance.add(item, priority);
-        System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
+        String expResult = "[(Jane Woods, 30), (Jeff Morrison, 20), (John Doe, 10)]";
+        String result = instance.toString();
+        assertEquals(expResult, result);
     }
     
     /**
      * Test of add method, of class SortedArrayPriorityQueue.
-     * Outputs current array string and head, then adds a new head and checks that the head function updates correctly.
+     * Adds a new head and checks that the head function updates correctly.
      */
     @Test
     public void testAddHead() throws Exception {
-        System.out.println("add");
+        System.out.println("Add new head");
         instance.add("John Doe", 10);
         instance.add("Jeff Morrison", 20);
-        System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
         Object item = ("Jane Woods");
         int priority = 30;
         instance.add(item, priority);
-        System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
+        String expResult = "Jane Woods";
+        String result = instance.head().toString();
+        assertEquals(expResult, result);
     }
     
     /**
@@ -83,16 +74,16 @@ public class SortedArrayPriorityQueueTest extends PriorityQueueTest{
      */
     @Test
     public void testAddEnd() throws Exception {
-        System.out.println("add");
+        System.out.println("Add new lowest priority item");
         instance.add("Jane Woods", 30);
         instance.add("Jeff Morrison", 20);
         System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
         Object item = "John Doe";
         int priority = 10;
         instance.add(item, priority);
-        System.out.println("Array: " + instance.toString());
-        System.out.println("Head: " + instance.head());
+        String expResult = "[(Jane Woods, 30), (Jeff Morrison, 20), (John Doe, 10)]";
+        String result = instance.toString();
+        assertEquals(expResult, result);
     }
      
     /**
@@ -103,7 +94,7 @@ public class SortedArrayPriorityQueueTest extends PriorityQueueTest{
     public void testToString() throws Exception {
         instance.add("John Doe", 10);
         instance.add("Jane Woods", 30);
-        System.out.println("toString");
+        System.out.println("Test toString method");
         String expResult = "[(Jane Woods, 30), (John Doe, 10)]";
         String result = instance.toString();
         assertEquals(expResult, result);
